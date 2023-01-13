772db8f75bc21ba5c92b6b39af839ab5255f0469
SalfaBella <salfabella64@gmail.com>
2023-01-13 10:44:42 +0700

    first commit

---
+++ app/src/main/java/com/example/tugas3notifikasi/Activity_after_notif.kt
@@ -0,0 +1,11 @@
+package com.example.tugas3notifikasi
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Activity_after_notif : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_after_notif)
+    }
+}
